9f7296678c04972c46a88bb2d0d65b43038ddf2e
ethanvu <ethanhuynhvu@gmail.com>
2018-02-16 02:39:05 -0500

    Update consent tests

--- welcome_consent/tests.py
+++ welcome_consent/tests.py
@@ -10,6 +10,13 @@ class PlayerBot(Bot):
         yield (views.WelcomePage)
         if self.case == "yes_consent":
             yield (views.ConsentPage, {"consent": True})
+            assert ("Thank you for choosing to participate in our study! If "
+                    "at any point in the study you choose to leave, you may do "
+                    "so without consequence.") in self.html
+            yield (views.Results)
         if self.case == "no_consent":
             yield (views.ConsentPage, {"consent": False})
-        yield (views.Results)
+            assert ("Thank you for considering our study, and we are sorry "
+                    "to see that you have opted out. Please click next to "
+                    "exit.") in self.html
+            yield (views.Results)
