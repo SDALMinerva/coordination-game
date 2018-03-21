347f1a5579781d429141dac18e4de6f59bcd5f85
ethanvu <ethanhuynhvu@gmail.com>
2018-03-21 16:16:22 -0400

    Update tour/tutorial steps

--- main/static/main/page-header-nav.js
+++ main/static/main/page-header-nav.js
@@ -1,7 +1,7 @@
 $('.page-header').append(
   '<ul id="help-nav" class="nav float-right">' +
   	'<li class="no-hover nav-item float-right">' +
-    	'<a class="nav-link" href="javascript:void(0);" onclick="startIntro();">Tour</a>' +
+    	'<a class="nav-link" href="javascript:void(0);">Tour</a>' +
   	'</li>'+
     '<li class="no-hover nav-item float-right">' + 
     	'<a class="nav-link" href="" data-toggle="modal" data-target="#InstructionsModal">Instructions</a>' +
@@ -9,5 +9,12 @@ $('.page-header').append(
     '<li class="no-hover nav-item float-right">' + 
     	'<a class="nav-link" href="" data-toggle="modal" data-target="#QuizModal">Quiz</a>'+
   	'</li>' +
-  	'</ul>'
+  '</ul>'
 );
+
+if ($('.otree-title').html().indexOf('Messaging') >= 0) {
+    $('#help-nav > .nav-item:first-child > a').attr('onclick', 'startDiscussTour()');
+}
+else if ($('.otree-title').html().indexOf('Decision') >= 0) {
+    $('#help-nav > .nav-item:first-child > a').attr('onclick', 'startDecideTour()');
+}
