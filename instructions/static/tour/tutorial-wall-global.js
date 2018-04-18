172945dd013593b97b74e6249b4960529dfe70d9
bjgoode <bjgoode@vt.edu>
2018-04-18 14:28:34 -0400

    update tour

--- instructions/static/tour/tutorial-wall-global.js
+++ instructions/static/tour/tutorial-wall-global.js
@@ -126,3 +126,6 @@ function startDiscussIntro() {
      
     intro.start();
   }
+  
+  
+  
--- main/static/main/page-header-nav.js
+++ main/static/main/page-header-nav.js
@@ -1,3 +1,4 @@
+if (output) {
 $('.page-header').append(
   '<ul id="help-nav" class="nav pull-right">' +
     '<li class="no-hover nav-item pull-right">' + 
@@ -7,7 +8,22 @@ $('.page-header').append(
     	'<a id="instructions-nav" class="nav-link clicktrack" href="" data-toggle="modal" data-target="#InstructionsModal">Instructions</a>' +
   	'</li>' +
   	'<li class="no-hover nav-item pull-right">' +
-    	'<a id="tour-nav" class="nav-link clicktrack" href="javascript:void(0);" onclick="startIntro();">Tour</a>' +
+    	'<a id="tour-nav" class="nav-link clicktrack" href="javascript:void(0);" onclick="startDiscussIntro();">Tour</a>' +
   	'</li>'+
   	'</ul>'
 );
+} else {
+$('.page-header').append(
+  '<ul id="help-nav" class="nav pull-right">' +
+    '<li class="no-hover nav-item pull-right">' + 
+    	'<a id="quiz-nav" class="nav-link clicktrack" href="" data-toggle="modal" data-target="#QuizModal">Quiz</a>'+
+  	'</li>' +
+    '<li class="no-hover nav-item pull-right">' + 
+    	'<a id="instructions-nav" class="nav-link clicktrack" href="" data-toggle="modal" data-target="#InstructionsModal">Instructions</a>' +
+  	'</li>' +
+  	'<li class="no-hover nav-item pull-right">' +
+    	'<a id="tour-nav" class="nav-link clicktrack" href="javascript:void(0);" onclick="startDecideIntro();">Tour</a>' +
+  	'</li>'+
+  	'</ul>'
+);
+}
