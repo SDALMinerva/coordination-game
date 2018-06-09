1896ec50595e122a85f0ca8b1c3253d820602227
Gizem Korkmaz <gkorkmaz@bi.vt.edu>
2018-06-09 17:05:41 -0400

    Update tutorial-none-local-game.js

--- instructions/static/tour/tutorial-none-local-game.js
+++ instructions/static/tour/tutorial-none-local-game.js
@@ -9,9 +9,7 @@
       {
         element: document.querySelectorAll('#playerDiv')[0],
         intro: "In each round, you will be assigned a randomly chosen <strong>identity (or avatar)</strong> and a <strong>threshold T</strong>. " +
-               "In each round, everyone will be assigned a new avatar and a new threshold. " +
-
-               "<br><br>You can click on your avatar at any time to see your own wall.",
+               "In each round, everyone will be assigned a new avatar and a new threshold. ",
       },
       {
         element: document.querySelectorAll('#reward-summary')[0],
