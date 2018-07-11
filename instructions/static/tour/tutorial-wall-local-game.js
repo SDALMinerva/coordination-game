9ab38c0f6eff6a5a50dc7e5f3230593d82e0f79d
Gizem Korkmaz <gkorkmaz@bi.vt.edu>
2018-07-11 13:44:32 -0400

    Update tutorial-wall-local-game.js

--- instructions/static/tour/tutorial-wall-local-game.js
+++ instructions/static/tour/tutorial-wall-local-game.js
@@ -106,10 +106,10 @@ function startDiscussIntro() {
       }, 
       {
         element: document.querySelectorAll('#participate-group')[0],
-        intro: "After reviewing the messages, each player in the group then must decide whether <strong>to participate or not " +
+        intro: "After reviewing the messages, each player in the group then must decide whether <strong>to participate</strong> or <strong>not " +
                "participate</strong> for this round. After making your participation decision for the round, a new round will start. You will not observe the outcome of the rounds. " +
                "<br><br>After you play all the rounds, we will choose a random round and you will be paid based on the " +
-               "results of that round. Because each round has the same chance of being chosen for payment, you should pay careful attention to " +
+               "result of that round. Because each round has the same chance of being chosen for payment, you should pay careful attention to " +
                "each round.",
       },
       /*
